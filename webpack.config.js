fc1dcef8dd935fd5058b86d248a38de2cd764919
k-a-v-g <kavyagoel09@gmail.com>
2022-07-17 19:04:19 +0530

    fourth commit

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,6 @@ module.exports = {
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'docs')
+        contentBase: path.join(__dirname, 'public')
     }
 };
